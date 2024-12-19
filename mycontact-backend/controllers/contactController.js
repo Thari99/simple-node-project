5662b91f0a75b332977ea34da219ee8c187c1376
Tharinda <tharindahasaranga99@gmail.com>
2024-12-20 02:21:21 +0530

    complete contact router with DB

--- mycontact-backend/controllers/contactController.js
+++ mycontact-backend/controllers/contactController.js
@@ -19,28 +19,55 @@ const createContact =asyncHandler(async(req, res)=>{
         res.status(400);
         throw new Error("All fields are mendatory !");
     }
-    res.status(201).json({message:"Create contacts"})
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    });
+    res.status(201).json(contact);
 });
 
 //@desc Get contacts
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async(req, res)=>{
-    res.status(200).json({message:`Get contacts for ${req.params.id}`})
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact)
 });
 
 //@desc Update contacts
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async(req, res)=>{
-    res.status(200).json({message:`Update contacts for ${req.params.id}`})
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    };
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );
+
+    res.status(200).json(updatedContact)
 });
 
 //@desc Delete contacts
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = asyncHandler(async(req, res)=>{
-    res.status(200).json({message:`Delete contacts for ${req.params.id}`})
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    await Contact.remove(); // This removes the document from the database
+    res.status(200).json(contact);
 });
 
 module.exports = {getContacts,
--- mycontact-backend/server.js
+++ mycontact-backend/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 // that help to display request body parms
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHnadler);
 
 app.listen(port,()=>{
