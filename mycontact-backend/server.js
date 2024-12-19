7e0f15f3695ac1543c28c18c93ab61e1ccd2e2c2
Tharinda <tharindahasaranga99@gmail.com>
2024-12-19 14:15:04 +0530

    partialy complite backend without DB

--- mycontact-backend/server.js
+++ mycontact-backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const errorHnadler = require("./middleware/errorhandler");
+const dotenv = require ("dotenv").config()
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+// that help to display request body parms
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHnadler);
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
